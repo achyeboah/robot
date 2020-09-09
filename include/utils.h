3e880a75c906c020554e422d6b5c146183581271
Sam <sam@debianVM>
2020-09-09 16:17:17 +0100

    good commit. Utils for read/writing imu data added

---
+++ include/utils.h
@@ -0,0 +1,17 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+namespace samsRobot{
+
+	struct imu_data{
+		unsigned int type;
+		float pitch, yaw, roll;
+		float wVelX, wVelY, wVelZ;
+		float head, temp;
+	};
+
+	unsigned int write_imu_data(const char* filename, const imu_data data);
+	unsigned int read_imu_data(const char* filename, imu_data& data);
+}
+
+#endif
---
+++ utils.cpp
@@ -0,0 +1,50 @@
+#include "utils.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+namespace samsRobot{
+
+	unsigned int read_imu_data(const char* filename, imu_data& data){
+
+		FILE* fhandle;
+		if (filename == NULL)
+			return 0;
+		fhandle = fopen(filename, "r");
+		if (fhandle == NULL)
+			return 0;
+
+		// parse the data into the object
+		fscanf(fhandle, "%d: %f %f %f; %f %f %f; %f %f", &(data.type), &(data.pitch), &(data.yaw), &(data.roll), &(data.wVelX), &(data.wVelY), &(data.wVelZ), &(data.head), &(data.temp));
+		// fscanf(fhandle, "%d: %f %f %f; %f %f %f; %f %f", &(type), &(pitch), &(yaw), &(roll), &(wVelX), &(wVelY), &(wVelZ), &(heading), &(temp));
+
+		return 0;
+
+	}
+
+	unsigned int write_imu_data(const char* filename, const imu_data data){
+		// open a handle to a local file and write to it
+		FILE *fhandle;
+		if(filename == NULL)
+			fhandle = fopen("imu_data.txt","w+");
+		else
+			fhandle = fopen(filename, "w+");
+
+		if(fhandle==NULL){
+			perror("file could not be opened\n");
+			return -1;
+		}
+
+		fprintf(fhandle, "%d: %f %f %f; %f %f %f; %f %f\n",
+				data.type == 0 ? 6050 : 9250,
+				data.pitch, data.yaw, data.roll,
+				data.wVelX, data.wVelY, data.wVelZ,
+				data.head,
+				data.temp);
+			   
+		fclose(fhandle);
+		return 0;
+	}
+
+}
+
