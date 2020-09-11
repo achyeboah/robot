c721744b5f99bf440b0e0bbb8b1f88c7958b9399
Sam <sam@debianVM>
2020-09-11 21:31:26 +0100

    minor changes to read/write data

--- src/utils.cpp
+++ src/utils.cpp
@@ -15,8 +15,13 @@ namespace samsRobot{
 			return 0;
 
 		// parse the data into the object
-		fscanf(fhandle, "%d: %f %f %f; %f %f %f; %f %f", &(data.type), &(data.pitch), &(data.yaw), &(data.roll), &(data.wVelX), &(data.wVelY), &(data.wVelZ), &(data.head), &(data.temp));
-		// fscanf(fhandle, "%d: %f %f %f; %f %f %f; %f %f", &(type), &(pitch), &(yaw), &(roll), &(wVelX), &(wVelY), &(wVelZ), &(heading), &(temp));
+		fscanf(fhandle, "%d: %f %f %f; %f %f %f; %f %f %f; %f", 
+				&(data.type), 
+				&(data.pitch), &(data.yaw), &(data.roll), 
+				&(data.wVelX), &(data.wVelY), &(data.wVelZ), 
+				&(data.headX), &(data.headY), &(data.headZ),
+			       	&(data.temp)
+				);
 
 		return 0;
 
@@ -35,11 +40,11 @@ namespace samsRobot{
 			return -1;
 		}
 
-		fprintf(fhandle, "%d: %f %f %f; %f %f %f; %f %f\n",
+		fprintf(fhandle, "%d: %f %f %f; %f %f %f; %f %f %f; %f\n",
 				data.type == 0 ? 6050 : 9250,
 				data.pitch, data.yaw, data.roll,
 				data.wVelX, data.wVelY, data.wVelZ,
-				data.head,
+				data.headX, data.headY, data.headZ,
 				data.temp);
 			   
 		fclose(fhandle);
